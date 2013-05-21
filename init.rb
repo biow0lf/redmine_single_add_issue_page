bffb8d32b50b4872b9ed6d219fc501837071d040
Igor Zubkov <icesik@altlinux.org>
2013-05-22 02:23:25 +0300

    Init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :single_add_issue_page do
+  name 'Single Add Issue Page plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
